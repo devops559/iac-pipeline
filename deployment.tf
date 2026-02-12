2fb770498dbbbc57a201cf728e840b837322b516
devops559 <devops@aioneq.com>
2026-02-12 16:37:27 +0530

    Update deployment.tf

--- deployment.tf
+++ deployment.tf
@@ -19,15 +19,79 @@ module "vpc" {
   }
 }
 # #ECS MODULE
-module "ecs" {
-  source  = "terraform-aws-modules/ecs/aws"
-  cluster_name = "dev-cluster"
+resource "aws_ecs_cluster" "this" {
+  name = var.cluster_name
 }
 
-resource "aws_lb" "dev_alb" {
-  name               = "dev-alb"
-  internal           = false
-  load_balancer_type = "application"
-  subnets            = module.vpc.public_subnets
-  security_groups    = [aws_security_group.alb_sg.id]
+resource "aws_ecs_task_definition" "app" {
+  family                   = "${var.cluster_name}-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = var.task_cpu
+  memory                   = var.task_memory
+  execution_role_arn       = var.execution_role_arn
+  task_role_arn            = var.task_role_arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "app-container"
+      image     = var.container_image
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+          protocol      = "tcp"
+        }
+      ]
+      environment = [
+        {
+          name  = "ENV"
+          value = "dev"
+        }
+      ]
+      secrets = [
+        {
+          name      = "DB_PASSWORD"
+          valueFrom = var.db_secret_arn
+        }
+      ]
+      mountPoints = [
+        {
+          sourceVolume  = "efs-volume"
+          containerPath = "/mnt/data"
+        }
+      ]
+    }
+  ])
+
+  volume {
+    name = "efs-volume"
+    efs_volume_configuration {
+      file_system_id = var.efs_id
+    }
+  }
 }
+
+resource "aws_ecs_service" "app" {
+  name            = "${var.cluster_name}-service"
+  cluster         = aws_ecs_cluster.this.id
+  task_definition = aws_ecs_task_definition.app.arn
+  desired_count   = var.desired_count
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = var.private_subnets
+    security_groups = [var.app_sg_id]
+    assign_public_ip = false
+  }
+
+  load_balancer {
+    target_group_arn = var.target_group_arn
+    container_name   = "app-container"
+    container_port   = 80
+  }
+
+  depends_on = [var.alb_listener]
+}
+
