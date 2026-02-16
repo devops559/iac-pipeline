2dc5103d746a6417ce81bc3a753376a6170bbc32
devops559 <devops@aioneq.com>
2026-02-16 10:52:03 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,13 @@
 terraform {
   required_version = ">= 1.3"
 
-  backend "s3" {}
+  backend "s3" {
+    bucket         = "my-dev-terraform-state"  
+    key            = "dev/terraform.tfstate"    
+    region         = "ap-south-1"               
+    dynamodb_table = "terraform-locks"          
+    encrypt        = true                       
+  }
 }
 
 provider "aws" {
