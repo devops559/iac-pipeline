00fcd681de27aee9658aa14074d28b9e52db67c8
devops559 <devops@aioneq.com>
2026-02-18 22:05:57 +0530

    Create ecs.tf

---
+++ ecs.tf
@@ -0,0 +1,62 @@
+resource "aws_ecs_cluster" "this" {
+  name = "${var.project}-cluster"
+}
+
+resource "aws_security_group" "ecs" {
+  vpc_id = aws_vpc.this.id
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_ecs_task_definition" "this" {
+  family                   = "${var.project}-task"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 256
+  memory                   = 512
+  execution_role_arn       = aws_iam_role.ecs_execution.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "app"
+      image     = var.container_image
+      essential = true
+      portMappings = [{
+        containerPort = 80
+        hostPort      = 80
+      }]
+    }
+  ])
+}
+
+resource "aws_ecs_service" "this" {
+  name            = "${var.project}-svc"
+  cluster         = aws_ecs_cluster.this.id
+  task_definition = aws_ecs_task_definition.this.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = aws_subnet.public[*].id
+    security_groups = [aws_security_group.ecs.id]
+    assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.this.arn
+    container_name   = "app"
+    container_port   = 80
+  }
+}
