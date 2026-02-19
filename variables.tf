6724b89aed2744a337afb1a58d5a5012ccad694d
devops559 <devops@aioneq.com>
2026-02-19 15:21:48 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,19 +1,70 @@
+# General Configuration
 variable "region" {
-  default = "ap-south-1"
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
 }
 
 variable "project" {
-  default = "iac"
+  description = "Project name"
+  type        = string
 }
 
+variable "environment" {
+  description = "Environment name (dev, beta, prod)"
+  type        = string
+}
+# Network Configuration
 variable "vpc_cidr" {
-  default = "10.10.0.0/16"
+  description = "VPC CIDR block"
+  type        = string
 }
 
 variable "public_subnets" {
-  default = ["10.10.1.0/24", "10.10.2.0/24"]
+  description = "List of public subnet CIDRs"
+  type        = list(string)
 }
 
+variable "private_subnets" {
+  description = "List of private subnet CIDRs"
+  type        = list(string)
+}
+
+variable "enable_nat_gateway" {
+  description = "Enable NAT Gateway"
+  type        = bool
+}
+
+variable "single_nat_gateway" {
+  description = "Use single NAT Gateway"
+  type        = bool
+  default     = true
+}
+# ECS Configuration
 variable "container_image" {
-  description = "ECR image"
+  description = "ECR container image URI"
+  type        = string
+}
+
+variable "task_cpu" {
+  description = "Fargate CPU units"
+  type        = number
+  default     = 256
+}
+
+variable "task_memory" {
+  description = "Fargate memory (MB)"
+  type        = number
+  default     = 512
+}
+
+variable "desired_count" {
+  description = "Number of ECS tasks"
+  type        = number
+  default     = 1
+}
+
+variable "assign_public_ip" {
+  description = "Assign public IP to ECS tasks"
+  type        = bool
 }
