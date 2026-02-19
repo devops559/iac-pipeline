f52c0e6ad667e494acab0435fa075af694df94c9
devops559 <devops@aioneq.com>
2026-02-19 15:20:46 +0530

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -1,8 +1,11 @@
+#ALB Security Group
 resource "aws_security_group" "alb" {
-  name   = "${var.project}-alb-sg"
-  vpc_id = aws_vpc.this.id
+  name        = "${var.project}-${var.environment}-alb-sg"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = aws_vpc.this.id
 
   ingress {
+    description = "Allow HTTP from internet"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
@@ -10,28 +13,52 @@ resource "aws_security_group" "alb" {
   }
 
   egress {
+    description = "Allow all outbound traffic"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Name = "${var.project}-${var.environment}-alb-sg"
+  }
 }
 
+# Application Load Balancer
 resource "aws_lb" "this" {
-  name               = "${var.project}-alb"
+  name               = "${var.project}-${var.environment}-alb"
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb.id]
   subnets            = aws_subnet.public[*].id
+
+  tags = {
+    Name = "${var.project}-${var.environment}-alb"
+  }
 }
 
+# Target Group
 resource "aws_lb_target_group" "this" {
-  name        = "${var.project}-tg"
+  name        = "${var.project}-${var.environment}-tg"
   port        = 80
   protocol    = "HTTP"
   vpc_id      = aws_vpc.this.id
-  target_type = "ip"
-}
+  target_type = "ip"   
 
+  health_check {
+    path                = "/"
+    matcher             = "200-399"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 3
+  }
+
+  tags = {
+    Name = "${var.project}-${var.environment}-tg"
+  }
+}
+# Listener
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.this.arn
   port              = 80
