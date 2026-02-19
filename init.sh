589b9704fbe7e1183785a5da41a908bededf7262
devops559 <devops@aioneq.com>
2026-02-19 15:49:17 +0530

    Update init.sh

--- init.sh
+++ init.sh
@@ -22,8 +22,8 @@ set -euo pipefail
 ENV="$1"
 
 terraform init \
-  -backend-config="bucket=adeeba-terraform-state-bucket-testing" \
+  -backend-config="bucket=terraform-statefile-testing-dev" \
   -backend-config="key=${ENV}/terraform.tfstate" \
   -backend-config="region=us-east-1" \
-  -backend-config="dynamodb_table=terraform-locks" \
+  -backend-config="dynamodb_table=terraform-testing-dev" \
   -backend-config="encrypt=true"
