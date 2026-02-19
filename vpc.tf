63743d20a1d377ee24d117f90266dd36622a32a2
devops559 <devops@aioneq.com>
2026-02-19 15:22:20 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,27 +1,42 @@
+# VPC
 resource "aws_vpc" "this" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
-  tags = { Name = "${var.project}-vpc" }
-}
 
+  tags = {
+    Name = "${var.project}-${var.environment}-vpc"
+  }
+}
+# Internet Gateway
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.this.id
-}
 
+  tags = {
+    Name = "${var.project}-${var.environment}-igw"
+  }
+}
+# Availability Zones
+data "aws_availability_zones" "available" {}
+# Public Subnets
 resource "aws_subnet" "public" {
-  count                   = 2
+  count                   = length(var.public_subnets)
   vpc_id                  = aws_vpc.this.id
   cidr_block              = var.public_subnets[count.index]
   map_public_ip_on_launch = true
   availability_zone       = data.aws_availability_zones.available.names[count.index]
-  tags = { Name = "${var.project}-public-${count.index}" }
+
+  tags = {
+    Name = "${var.project}-${var.environment}-public-${count.index}"
+  }
 }
-
-data "aws_availability_zones" "available" {}
-
+# Public Route Table
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "${var.project}-${var.environment}-public-rt"
+  }
 }
 
 resource "aws_route" "public_internet" {
@@ -31,7 +46,63 @@ resource "aws_route" "public_internet" {
 }
 
 resource "aws_route_table_association" "public" {
-  count          = 2
+  count          = length(var.public_subnets)
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
+}
+# Private Subnets
+resource "aws_subnet" "private" {
+  count                   = length(var.private_subnets)
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.private_subnets[count.index]
+  map_public_ip_on_launch = false
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name = "${var.project}-${var.environment}-private-${count.index}"
+  }
+}
+# NAT Gateway 
+resource "aws_eip" "nat" {
+  count = var.enable_nat_gateway ? 1 : 0
+  vpc   = true
+
+  tags = {
+    Name = "${var.project}-${var.environment}-nat-eip"
+  }
+}
+
+resource "aws_nat_gateway" "this" {
+  count         = var.enable_nat_gateway ? 1 : 0
+  allocation_id = aws_eip.nat[0].id
+  subnet_id     = aws_subnet.public[0].id
+
+  depends_on = [aws_internet_gateway.igw]
+
+  tags = {
+    Name = "${var.project}-${var.environment}-nat"
+  }
+}
+
+# Private Route Table
+resource "aws_route_table" "private" {
+  count  = var.enable_nat_gateway ? 1 : 0
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "${var.project}-${var.environment}-private-rt"
+  }
+}
+
+resource "aws_route" "private_nat" {
+  count                  = var.enable_nat_gateway ? 1 : 0
+  route_table_id         = aws_route_table.private[0].id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.this[0].id
+}
+
+resource "aws_route_table_association" "private" {
+  count          = var.enable_nat_gateway ? length(var.private_subnets) : 0
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[0].id
 }
